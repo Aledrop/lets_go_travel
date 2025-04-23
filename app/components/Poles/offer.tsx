a2438b8be28d649cdf92ae46650bcdb627a8676c
Aledrop <85901135+Aledrop@users.noreply.github.com>
2025-04-23 14:07:10 -0400

    arreglo express

--- app/components/Poles/offer.tsx
+++ app/components/Poles/offer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Offer() {
   return (
-    <div className="relative flex justify-between bg-orange-500 h-[720px] text-white rounded-[4.5rem] md:rounded-tl-none">
+    <div className="relative flex justify-between bg-orange-500 h-[800px] text-white rounded-[4.5rem] md:rounded-tl-none">
       <h2 className="md:absolute top-3 md:left-3 px-10 py-4 z-20 md:text-7xl text-5xl font-bold uppercase">
         OFERTAS PAQUETES
       </h2>
@@ -15,14 +15,14 @@ export default function Offer() {
       <Image
         src="/images/ele_map.png"
         alt="Familia viajando"
-        width={300}
+        width={350}
         height={100}
         className="absolute bottom-0 right-0 z-10"
       />
       <Image
         src="/images/familia.png"
         alt="Familia viajando"
-        width={300}
+        width={400}
         height={100}
         className="absolute bottom-0 left-0 z-20"
       />
